d7bde821c083d5e34d9928847d4f219fc9865d35
jujae03 <0719improvement@naver.com>
2024-07-24 16:00:01 +0900

    Github 토큰만 사용하여 repository 나오게 수정

--- src/main/java/com/bringup/member/github/controller/GithubController.java
+++ src/main/java/com/bringup/member/github/controller/GithubController.java
@@ -17,9 +17,9 @@ public class GithubController {
         return gitHubService.getUserData(githubToken);
     }
 
-    @GetMapping("/github/orgs/{org}/repos")
-    public String getOrgRepos(@PathVariable String org, @RequestParam("token") String githubToken) {
-        System.out.println("token: " + githubToken);
-        return gitHubService.getOrgRepos(org, githubToken);
+    @GetMapping("/github/user/repos")
+    public String getUserRepos(@RequestParam("token") String githubToken) {
+        String userLogin = gitHubService.getUserLogin(githubToken);
+        return gitHubService.getOrgRepos(userLogin, githubToken);
     }
 }
