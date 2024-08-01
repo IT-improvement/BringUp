76f00662a365f36034d3399488e5215b9885ec93
LBJ <pojun406@naver.com>
2024-08-02 01:25:39 +0900

    create Company, Member Filter

--- src/main/java/com/bringup/common/jwt/CompanyLoginFilter.java
+++ src/main/java/com/bringup/common/jwt/CompanyLoginFilter.java
@@ -14,63 +14,47 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import java.util.Collection;
 import java.util.Iterator;
 
-public class LoginFilter extends UsernamePasswordAuthenticationFilter {
+public class CompanyLoginFilter extends UsernamePasswordAuthenticationFilter {
 
     private final AuthenticationManager authenticationManager;
     private final JWTUtil jwtUtil;
 
-    public LoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
-
+    public CompanyLoginFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
         this.authenticationManager = authenticationManager;
         this.jwtUtil = jwtUtil;
+        setFilterProcessesUrl("/company/login"); // Company 엔드포인트 설정
     }
+
     @Override
     protected String obtainUsername(HttpServletRequest request) {
-        // 클라이언트 요청에서 userEmail 파라미터 추출
         return request.getParameter("userEmail");
     }
 
     @Override
     protected String obtainPassword(HttpServletRequest request) {
-        // 클라이언트 요청에서 userPassword 파라미터 추출
         return request.getParameter("userPassword");
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
-
-        //클라이언트 요청에서 username, password 추출
         String userEmail = obtainUsername(request);
         String password = obtainPassword(request);
-
-        //스프링 시큐리티에서 username과 password를 검증하기 위해서는 token에 담아야 함
         UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userEmail, password, null);
-
-        //token에 담은 검증을 위한 AuthenticationManager로 전달
         return authenticationManager.authenticate(authToken);
     }
 
-    //로그인 성공시 실행하는 메소드 (여기서 JWT를 발급하면 됨)
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
-        //UserDetailsS
         CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
-
         String userEmail = customUserDetails.getUsername();
-
         Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
         Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
         GrantedAuthority auth = iterator.next();
-
         String role = auth.getAuthority();
-
-        //토큰 생성
-        String token = jwtUtil.createJwt(userEmail, role, 60*60*10L);
-        //Authorization토큰에 담고 인증방식을 접두사 "Bearer " 뒤에 띄어쓰기 필수
+        String token = jwtUtil.createJwt(userEmail, role, 60 * 60 * 10L);
         response.addHeader("Authorization", "Bearer " + token);
     }
 
-    //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
         response.setStatus(401);
