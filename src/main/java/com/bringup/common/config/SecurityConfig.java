14e45d48d7f5150c6b8563b3a58d93e7e9eb8efb
LBJ <pojun406@naver.com>
2024-08-02 01:25:39 +0900

    create Company, Member Filter

--- src/main/java/com/bringup/common/config/SecurityConfig.java
+++ src/main/java/com/bringup/common/config/SecurityConfig.java
@@ -2,7 +2,8 @@ package com.bringup.common.config;
 
 import com.bringup.common.jwt.JWTFilter;
 import com.bringup.common.jwt.JWTUtil;
-import com.bringup.common.jwt.LoginFilter;
+import com.bringup.common.jwt.MemberLoginFilter;
+import com.bringup.common.jwt.CompanyLoginFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,8 +16,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity //이클래스가 security config를 수정한다는 애너테이션
-
+@EnableWebSecurity
 public class SecurityConfig {
 
     private final AuthenticationConfiguration authenticationConfiguration;
@@ -44,42 +44,13 @@ public class SecurityConfig {
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/", "/join").permitAll()
+                        .requestMatchers("/member/login", "/company/login", "/", "/join").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(new JWTFilter(jwtUtil), UsernamePasswordAuthenticationFilter.class)
-                .addFilterAt(new LoginFilter(authenticationManager(), jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(new MemberLoginFilter(authenticationManager(), jwtUtil), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(new CompanyLoginFilter(authenticationManager(), jwtUtil), UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
     }
 }
-
-/*
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-
-        http //동작 순서는 상단부터 , 스프링 버전 3.1.x 일때는 무조건 람다 형식으로 메서드를 나열해야한다.
-                .authorizeHttpRequests(auth -> auth //authorizeHttpRequests는 특정한 경로에 대해 요청을 허용학나 거부한다. 람다식 꼭사용해야함
-                                .requestMatchers("/", "/phLogin", "/join", "/joinProc").permitAll() //특정한 경로에 요청을 진행할 수 있는 메서드
-                                //permitAll : 모든 사용자에게 로그인을 하지않아도 접근 가능하게 해주는 메서드,
-                                .requestMatchers("/admin").hasRole("ADMIN") //admin이면 접근가능
-                                //hasRole : 특정한 규칙이 있어야 규칙이 있어야 경로에 접근가능
-                                .requestMatchers("/member/**").hasAnyRole("ADMIN", "USER")
-                                //hasAnyRole : 여러가지 규칙 설정가능
-                                .anyRequest().authenticated()
-                        //authenticated : 로그인만 진행되면 접근 가능
-                        //denyAll : 로그인을 해도 접근못하게 하는 메서드
-                )
-                .formLogin(auth -> auth
-                        .loginPage("/phLogin") //스프링 시큐리티가 자동으로 login으로 화면을 보여준다.
-                        .loginProcessingUrl("/loginProc") //로그인페이지와 더불어서 loginProc 안에 프론트에서 넘긴 id 비밀번호를 시큐리티가 받아서 로그인 처리를 해준다.
-                        .permitAll() //그 경로로 아무나 들어올수 있게 설정
-                        .defaultSuccessUrl("/", true) // '/'로 설정하여 오류 해결
-                );
-
-        http
-                .csrf(auth -> auth.disable());
-
-        return http.build(); //받은 http를 build타입으로 반환
-    }*/
-
