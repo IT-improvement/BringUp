cb842616497989acd9cf9366793e9a77f8e5cf01
pjh5020 <123628540+pjh5020@users.noreply.github.com>
2024-08-13 16:53:47 +0900

    add : 주석추가

--- src/main/java/com/bringup/member/user/controller/LoginController.java
+++ src/main/java/com/bringup/member/user/controller/LoginController.java
@@ -28,13 +28,21 @@ public class LoginController {
         return "member/user/login";
     }
 
+    /* 클라이언트의 로그인 요청
+    클라이언트가 사용자의 이메일(userEmail)과 비밀번호(userPassword)를 POST 요청으로 /member/userLogin 엔드포인트에 보냅니다.
+     */
     @PostMapping("/userLogin")
     public ResponseEntity<BfResponse<UserLoginTokenDTO>> login(@Valid @RequestBody UserLoginDTO loginDTO) {
         // 디버그 로그 추가
         System.out.println("Controller: login method called with " + loginDTO.getUserEmail());
         return ResponseEntity.ok(new BfResponse<>(userLoginService.login(loginDTO)));
+        /*  로직 설명
+        1.LoginController에서 로그인 요청 처리
+    LoginController의 login 메서드가 요청을 받습니다.
+    login 메서드는 전달된 UserLoginDTO 객체(로그인 정보가 포함된 DTO)를 사용하여 UserLoginService의 login 메서드를 호출합니다.
+    LoginController는 UserLoginService로부터 반환된 UserLoginTokenDTO를 클라이언트에 응답합니다.
+     */
     }
-
     // Email 중복 체크
     @PostMapping("/checkEmail")
     public ResponseEntity<BfResponse<?>> checkEmail(@RequestBody Map<String, String> requestBody) {
