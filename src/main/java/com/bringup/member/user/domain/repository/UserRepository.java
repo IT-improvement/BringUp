11720284c952727ab81ef31c1ed2ae2b126bbb40
pjh5020 <123628540+pjh5020@users.noreply.github.com>
2024-08-15 04:11:34 +0900

    modify : userJoin.js , delete :  userCheckId.js
    userCheckId.js ->  userJoin.js 으로 변경
    오류 수정 완료 및 회원가입 순서 설정

--- src/main/java/com/bringup/member/user/domain/repository/UserRepository.java
+++ src/main/java/com/bringup/member/user/domain/repository/UserRepository.java
@@ -2,9 +2,10 @@ package com.bringup.member.user.domain.repository;
 
 import com.bringup.member.user.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UserRepository extends JpaRepository<UserEntity, Integer> {
     Boolean existsByUserEmail(String userEmail);
     Optional<UserEntity> findByUserEmail(String userEmail); //Optional을 사용함으로써 null 값을 처리하는 데 따른 문제를 보다 명확하게 할 수 있음
