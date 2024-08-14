153f9e92bb48ebac5abe0f2cdc3b35ecd706d4f0
주승재 <0719improvement@naver.com>
2024-08-14 16:22:51 +0900

    Modify GithubRepository filetype class->interface

--- src/main/java/com/bringup/member/portfolio/github/domain/GithubRepository.java
+++ src/main/java/com/bringup/member/portfolio/github/domain/GithubRepository.java
@@ -1,4 +1,8 @@
 package com.bringup.member.portfolio.github.domain;
 
-public class GithubRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GithubRepository extends JpaRepository<GithubEntity, Integer> {
 }
