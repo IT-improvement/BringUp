a8f42757e23a261f6697f768443a3090a244a9ba
주승재 <0719improvement@naver.com>
2024-08-16 15:53:42 +0900

    File 이동 : Portfolio

--- src/main/java/com/bringup/member/portfolio/portfolio/domain/PortfolioRepository.java
+++ src/main/java/com/bringup/member/portfolio/portfolio/domain/PortfolioRepository.java
@@ -1,4 +1,4 @@
-package com.bringup.member.portfolio.portfolio;
+package com.bringup.member.portfolio.portfolio.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
