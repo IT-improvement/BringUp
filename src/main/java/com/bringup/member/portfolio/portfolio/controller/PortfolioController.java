3d9ca6f65c6417dcbac14b19d0540e5bb7389cf0
주승재 <0719improvement@naver.com>
2024-08-16 17:43:27 +0900

    Add PortfolioController: githubinsert method

--- src/main/java/com/bringup/member/portfolio/portfolio/controller/PortfolioController.java
+++ src/main/java/com/bringup/member/portfolio/portfolio/controller/PortfolioController.java
@@ -1,14 +1,14 @@
 package com.bringup.member.portfolio.portfolio.controller;
 
+import com.bringup.common.response.ResponseDto;
 import com.bringup.member.portfolio.portfolio.domain.PortfolioService;
+import com.bringup.member.portfolio.portfolio.dto.PortfolioInsertResponseDto;
 import com.bringup.member.portfolio.portfolio.dto.PortfolioReponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/portfolio")
@@ -21,5 +21,11 @@ public class PortfolioController {
     public ResponseEntity<? super PortfolioReponseDto> getList(@AuthenticationPrincipal String userCode){
         ResponseEntity<? super PortfolioReponseDto> respone = portfolioService.portfolioList(userCode);
         return respone;
+    }
+
+    @PostMapping("/github")
+    public ResponseEntity<? super PortfolioInsertResponseDto> insertGithub(@AuthenticationPrincipal String userCode){
+
+        return PortfolioInsertResponseDto.success();
     }
 }
