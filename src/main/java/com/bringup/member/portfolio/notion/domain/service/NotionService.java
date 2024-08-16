8a141d40b2d633c1145ed983d6c9c6f9f357c40d
주승재 <0719improvement@naver.com>
2024-08-16 15:54:06 +0900

    Add NotionService: Notion API 추가

--- src/main/java/com/bringup/member/portfolio/notion/domain/service/NotionService.java
+++ src/main/java/com/bringup/member/portfolio/notion/domain/service/NotionService.java
@@ -1,7 +1,30 @@
 package com.bringup.member.portfolio.notion.domain.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
+@RequiredArgsConstructor
 public class NotionService {
+
+    private final RestTemplate restTemplate;
+
+
+    public String getUserData(String notionToken){
+        String url = "https://api.notion.com/v1";
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "token " + notionToken);
+        headers.set("Accept", "application/vnd.github.v3+json");
+
+        HttpEntity<String> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
+        return response.getBody();
+    }
 }
