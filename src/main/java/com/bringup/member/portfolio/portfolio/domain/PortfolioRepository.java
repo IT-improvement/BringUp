b5270de115899363f66a0c3963d411576c34883f
주승재 <0719improvement@naver.com>
2024-08-16 16:44:26 +0900

    Add PortfolioRepository: findByUserIndex method 추가

--- src/main/java/com/bringup/member/portfolio/portfolio/domain/PortfolioRepository.java
+++ src/main/java/com/bringup/member/portfolio/portfolio/domain/PortfolioRepository.java
@@ -3,6 +3,9 @@ package com.bringup.member.portfolio.portfolio.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+
 @Repository
 public interface PortfolioRepository extends JpaRepository<PortfolioEntity, Integer> {
+    ArrayList findByUserIndex(String userIndex);
 }
