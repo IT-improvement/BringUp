1c935d918b060214269f7ddb62e907f20e685085
주승재 <0719improvement@naver.com>
2024-08-18 12:15:35 +0900

    Add HomeController: root 경로 jsp 맵핑

--- src/main/java/com/bringup/HomeController.java
+++ src/main/java/com/bringup/HomeController.java
@@ -5,4 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
+    @GetMapping("")
+    public String main(){
+        return "common/main_ex";
+    }
 }
