888259a2cb5f0a2238dd1ea4e263a5f18bc46e46
주승재 <0719improvement@naver.com>
2024-08-19 17:23:43 +0900

    Add ImageService: upLoadImage Method 추가

--- src/main/java/com/bringup/common/image/ImageService.java
+++ src/main/java/com/bringup/common/image/ImageService.java
@@ -4,14 +4,45 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.util.UUID;
 
 @Service
 public class ImageService {
-    
+
     private static final Logger logger = LoggerFactory.getLogger(ImageService.class);
 
     @Value("${file.path}")
     private String filePath;
     @Value("${file.url}")
     private String fileUrl;
+
+    public String upLoadImage(MultipartFile file){
+        String savePath = saveImage(file);
+        return savePath;
+    }
+    // 이미지 저장
+    private String saveImage(MultipartFile file){
+        if(file.isEmpty()){
+            logger.error("not images");
+            return null;
+        }
+        String originalFileName = file.getOriginalFilename();
+        //확장자 추출
+        String extension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        String uuid = UUID.randomUUID().toString();
+        String saveFileName = uuid + extension;
+        String savePath = filePath+saveFileName;
+
+        try {
+            file.transferTo(new File(savePath));
+        }catch (Exception e){
+            logger.error("error save images");
+            e.printStackTrace();
+            return null;
+        }
+        return fileUrl+saveFileName;
+    }
 }
