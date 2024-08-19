000ae8a1bf9a0461f70260231ef9d3dc4e2c8123
주승재 <0719improvement@naver.com>
2024-08-19 17:22:38 +0900

    Create ImageController

---
+++ src/main/java/com/bringup/common/image/ImageController.java
@@ -0,0 +1,14 @@
+package com.bringup.common.image;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/image")
+@RequiredArgsConstructor
+public class ImageController {
+
+}
