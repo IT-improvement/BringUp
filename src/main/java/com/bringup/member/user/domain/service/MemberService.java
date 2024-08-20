c69d8e0f3f379298c52f1c84bb960ceaa93c142d
junsung1202 <junsung101010@gmail.com>
2024-08-20 23:01:48 +0900

    MemberService 추가
    회원정보 수정에 필요한 MemberService 추가, 여기서도 Optional 오류 발생

---
+++ src/main/java/com/bringup/member/user/domain/service/MemberService.java
@@ -0,0 +1,46 @@
+package com.bringup.member.user.domain.service;
+
+import com.bringup.member.user.domain.entity.UserEntity;
+import com.bringup.member.user.domain.repository.UserRepository;
+import com.bringup.member.user.dto.JoinDTO;
+import com.bringup.member.user.dto.MemberUpdateDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+@Log4j
+public class MemberService implements UserDetailsService{
+    private final UserRepository userRepository;
+    private final JoinService joinService;
+    private final UserLoginService userLoginService;
+
+    public int updateMember(MemberUpdateDto memberUpdateDto){
+        //Optional 오류 (이유 모름)
+        UserEntity userEntity = userRepository.findByUserEmail(memberUpdateDto.getUserEmail());
+        userEntity.updateUserEmail(memberUpdateDto.getUserEmail());
+        userEntity.updateUserName(memberUpdateDto.getUserName());
+        userEntity.updatePassword(memberUpdateDto.getUserPassword());
+        userEntity.updateUserAddress(memberUpdateDto.getUserAddress());
+        userEntity.updateUserBirthday(memberUpdateDto.getUserBirthday());
+        userEntity.updateUserPhoneNumber(memberUpdateDto.getUserPhoneNumber());
+        userEntity.updateStatus(memberUpdateDto.getStatus());
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String encodePW = encoder.encode(memberUpdateDto.getUserPassword());
+        userEntity.updatePassword(encodePW);
+        userRepository.save(userEntity);
+        return userEntity.getUserIndex();
+    }
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
