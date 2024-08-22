f71866f5fa061065d207f016618175229b76bd6f
LBJ <pojun406@naver.com>
2024-08-22 17:36:04 +0900

    type int -> Integer
    일단 Integer로 전부 바꿔봄

--- src/main/java/com/bringup/company/advertisement/dto/request/AdvertisementUploadRequestDto.java
+++ src/main/java/com/bringup/company/advertisement/dto/request/AdvertisementUploadRequestDto.java
@@ -5,6 +5,6 @@ import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class AdvertisementUploadRequestDto {
-    private int advertisementIndex;
+    private Integer advertisementIndex;
     private MultipartFile image;
 }
