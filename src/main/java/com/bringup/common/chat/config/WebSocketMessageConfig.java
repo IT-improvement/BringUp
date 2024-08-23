33d27a758ad6fbcc464a3a43f239e10880dc699e
주승재 <0719improvement@naver.com>
2024-08-23 14:12:08 +0900

    Add WebSocketMessageConfig: registerStompEndpoints method 추가

--- src/main/java/com/bringup/common/chat/config/WebSocketMessageConfig.java
+++ src/main/java/com/bringup/common/chat/config/WebSocketMessageConfig.java
@@ -1,6 +1,19 @@
 package com.bringup.common.chat.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketMessageConfig implements WebSocketMessageBrokerConfigurer {
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // stomp 접속 url -> /ws-stomp
+        registry.addEndpoint("ws-steomp")//연결될 엔드포인트
+                .withSockJS(); //SocketJS를 연결한다는 설정
+    }
+
 }
