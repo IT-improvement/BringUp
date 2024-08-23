d9f5e1ecc98ba1d10332292ee394cb9670aa3ba6
주승재 <0719improvement@naver.com>
2024-08-23 14:08:11 +0900

    Create WebSocketMessageConfig

---
+++ src/main/java/com/bringup/common/chat/config/WebSocketMessageConfig.java
@@ -0,0 +1,6 @@
+package com.bringup.common.chat.config;
+
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+public class WebSocketMessageConfig implements WebSocketMessageBrokerConfigurer {
+}
