47ecf1e9f1d58dbbfbd061b9bb792fe68c5c0764
LBJ <pojun406@naver.com>
2024-08-23 23:11:28 +0900

    recruitment add Image
    공고쪽에 이미지를 추가할수있게 만들어놨다.

--- src/main/java/com/bringup/company/recruitment/dto/response/RecruitmentResponseDto.java
+++ src/main/java/com/bringup/company/recruitment/dto/response/RecruitmentResponseDto.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 public class RecruitmentResponseDto {
     private Integer recruitmentIndex;
     private String recruitmentTitle;
-    private String recruitment_img;
+    private String recruitmentImg;
     private String managerEmail;
     private RecruitmentType recruitmentType; // 정규직, 비정규직, 아르바이트 중 하나
     private String category;
@@ -21,5 +21,4 @@ public class RecruitmentResponseDto {
     private String startDate;
     private String period;
     private String status;
-    private String recruitmentClass;
 }
