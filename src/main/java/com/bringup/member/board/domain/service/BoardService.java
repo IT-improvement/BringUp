9881e1874b140616d2f0a51fe2a9bf863180c877
junsung1202 <junsung101010@gmail.com>
2024-08-23 23:10:59 +0900

    Board 로직 추가, 수정
    board에 삭제 기능 추가, success dto 추가, mapping 수정

--- src/main/java/com/bringup/member/board/domain/service/BoardService.java
+++ src/main/java/com/bringup/member/board/domain/service/BoardService.java
@@ -4,6 +4,7 @@ import com.bringup.member.board.domain.entity.BoardEntity;
 import com.bringup.member.board.domain.repository.BoardRepository;
 import com.bringup.member.board.dto.request.BoardRequestDto;
 import com.bringup.member.board.dto.response.BoardResponseDto;
+import com.bringup.member.board.dto.response.SuccessResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -44,5 +45,17 @@ public class BoardService {
         }
         boardEntity.updatePost(boardRequestDto);
         return new BoardResponseDto(boardEntity);
+    }
+
+    @Transactional
+    public SuccessResponseDto deletePost(int userIndex, BoardRequestDto boardRequestDto) throws Exception{
+        BoardEntity boardEntity = boardRepository.findById(userIndex).orElseThrow(
+                () -> new IllegalArgumentException("아이디가 존재하지 않습니다.")
+        );
+        if (!boardRequestDto.getUserEmail().equals(boardEntity.getUserEmail())){
+            throw new Exception("이메일이 일치하지 않습니다.");
+        }
+        boardRepository.deleteById(userIndex);
+        return new SuccessResponseDto(true);
     }
 }
