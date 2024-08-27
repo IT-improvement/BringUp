7773bdf46a41381e801eea5c91e6eaddd08ae4a7
pjh5020 <123628540+pjh5020@users.noreply.github.com>
2024-08-27 20:14:41 +0900

    add : membership
    멤버십가입 로직 설계

---
+++ src/main/java/com/bringup/member/membership/domain/entity/UserMembership.java
@@ -0,0 +1,22 @@
+package com.bringup.member.membership.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "user_membership")
+public class UserMembership {
+
+    @Id
+    @Column(name = "user_index", nullable = false)
+    private Long userIndex;
+
+    @Column(name = "start_date", nullable = false, length = 30)
+    private String startDate;
+
+    @Column(name = "period", nullable = false, length = 30)
+    private String period;
+}
