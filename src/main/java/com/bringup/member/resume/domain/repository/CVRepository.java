9492b76eb7f0dafecaae160bf83521b1c00c837f
LBJ <pojun406@naver.com>
2024-08-27 23:48:25 +0900

    modify : Headhunt Logic
    헤드헌팅쪽에 멤버십유저를 위쪽으로 띄워주는 로직을 까먹고있었다. 이 부분의 로직을 추가했다.

--- src/main/java/com/bringup/member/resume/domain/repository/CVRepository.java
+++ src/main/java/com/bringup/member/resume/domain/repository/CVRepository.java
@@ -4,7 +4,11 @@ import com.bringup.member.resume.domain.entity.CVEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CVRepository extends JpaRepository<CVEntity, Integer> {
     CVEntity findByCvIndex(int cvIndex);
+
+    List<CVEntity> findAllByUserIndex(int index);
 }
