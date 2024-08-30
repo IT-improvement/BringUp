6a5ba093e16024e165d60eb586142b11709bea50
junsung1202 <junsung101010@gmail.com>
2024-08-31 00:23:34 +0900

    companybookmark 수정
    service, entity, dto 내용 수정
    service 북마크 추가, 리스트 출력, 북마크 삭제 로직 추가
    entity 요소 수정
    dto request, response 추가
    controller 추가

--- src/main/java/com/bringup/member/companybookmark/dto/request/CompanyBookMarkRequestDto.java
+++ src/main/java/com/bringup/member/companybookmark/dto/request/CompanyBookMarkRequestDto.java
@@ -5,7 +5,8 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class CompanyBookMarkDto {
+public class CompanyBookMarkRequestDto {
     private int userIndex;
     private int companyIndex;
+    private String status;
 }
