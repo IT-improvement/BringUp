9ad9acdb16180b2e2340f6f094624d37dccff554
junsung1202 <junsung101010@gmail.com>
2024-08-31 00:23:34 +0900

    companybookmark 수정
    service, entity, dto 내용 수정
    service 북마크 추가, 리스트 출력, 북마크 삭제 로직 추가
    entity 요소 수정
    dto request, response 추가
    controller 추가

--- src/main/java/com/bringup/member/companybookmark/domain/entity/CompanyBookMarkEntity.java
+++ src/main/java/com/bringup/member/companybookmark/domain/entity/CompanyBookMarkEntity.java
@@ -1,22 +1,22 @@
 package com.bringup.member.companybookmark.domain.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.bringup.member.companybookmark.dto.request.CompanyBookMarkRequestDto;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity(name = "company_bookmark")
-@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@NoArgsConstructor
 @Getter
 @Setter
+@Table(name = "company_bookmark")
 public class CompanyBookMarkEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int companyIndex;
+
     private int userIndex;
     private String status;
 }
