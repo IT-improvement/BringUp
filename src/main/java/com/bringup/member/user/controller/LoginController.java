aeb888fe4194e48dbdb3207c5b1846d34bbc660f
pjh5020 <123628540+pjh5020@users.noreply.github.com>
2024-09-04 11:00:23 +0900

    modify : recruitment filter
    직군 직무 공고 필터 수정

--- src/main/java/com/bringup/member/user/controller/LoginController.java
+++ src/main/java/com/bringup/member/user/controller/LoginController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Map;
+
 
 @Controller
 @RequestMapping("/member")
