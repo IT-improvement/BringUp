ae79cf34911eb983f75d78769300b17d6c5f3729
junsung1202 <junsung101010@gmail.com>
2024-09-13 02:40:10 +0900

    companybookmark 수정
    companybookmark service, controller 수정된 db에 맞게 로직 변경, MemberErrorCode에 사용자가 북마크한 기업이 없을 때 발생하는 오류코드 NOT_FOUND_BOOKMARK 추가

--- src/main/java/com/bringup/common/bookmark/domain/service/CompanyBookMarkService.java
+++ src/main/java/com/bringup/common/bookmark/domain/service/CompanyBookMarkService.java
@@ -22,6 +22,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import static com.bringup.common.enums.MemberErrorCode.NOT_FOUND_BOOKMARK;
 import static com.bringup.common.enums.MemberErrorCode.NOT_FOUND_MEMBER_ID;
 
 @Service
@@ -32,43 +33,38 @@ public class CompanyBookMarkService {
     private final CompanyRepository companyRepository;
     private final CVRepository cvRepository;
 
-    /*@Transactional
-    public CompanyBookMarkResponseDto addCompanyBookMark(CompanyBookMarkRequestDto companyBookMarkRequestDto){
-        UserEntity userEntity = userRepository.findByUserIndex(companyBookMarkRequestDto.getUserIndex())
-                .orElseThrow(()->new RuntimeException("사용자를 찾을 수 없습니다."));
+    public void addCompany(UserDetailsImpl userDetails){
+        UserEntity user = userRepository.findById(userDetails.getId())
+                .orElseThrow(()->new BookmarkException(NOT_FOUND_MEMBER_ID));
 
-        Company company = companyRepository.findBycompanyId(companyBookMarkRequestDto.getCompanyIndex())
-                .orElseThrow(()->new RuntimeException("기업을 찾을 수 없습니다."));
+        Company company = companyRepository.findById(userDetails.getId())
+                .orElseThrow(()->new BookmarkException(NOT_FOUND_MEMBER_ID));
 
-        Optional<CompanyBookMarkEntity> exitingBookMark = companyBookMarkRepository.findByUserIndexAndCompanyIndex(companyBookMarkRequestDto.getUserIndex(), companyBookMarkRequestDto.getCompanyIndex());
-        CompanyBookMarkEntity companyBookMarkEntity;
-
-        if (exitingBookMark.isPresent()){
-            companyBookMarkEntity = exitingBookMark.get();
-            companyBookMarkEntity.setStatus(BookmarkType.BOOKMARK);
-        } else {
-            companyBookMarkEntity = new CompanyBookMarkEntity();
-            companyBookMarkEntity.setCompany(company);
-            companyBookMarkEntity.setUser(userEntity);
-            companyBookMarkEntity.setStatus(BookmarkType.BOOKMARK);
-        }
-        CompanyBookMarkEntity saveBookMark = companyBookMarkRepository.save(companyBookMarkEntity);
-        return new CompanyBookMarkResponseDto(saveBookMark);
+        CompanyBookMarkEntity companyBookMarkEntity = new CompanyBookMarkEntity();
+        companyBookMarkEntity.setUser(user);
+        companyBookMarkEntity.setCompany(company);
+        companyBookMarkEntity.setStatus(BookmarkType.BOOKMARK);
+        companyBookMarkRepository.save(companyBookMarkEntity);
     }
 
-    public List<CompanyBookMarkResponseDto> getCompanyBookMarks(int userIndex){
-        List<CompanyBookMarkEntity> companyBookMarkEntityList = companyBookMarkRepository.findByUserIndexAndStatus(userIndex, "등록");
-        return companyBookMarkEntityList.stream()
-                .map(CompanyBookMarkResponseDto::new)
+    public List<CompanyBookMarkResponseDto> companyBookMarkList(UserDetailsImpl userDetails){
+        UserEntity user = userRepository.findByUserIndex(userDetails.getId())
+                .orElseThrow(()->new BookmarkException(NOT_FOUND_MEMBER_ID));
+
+        List<CompanyBookMarkEntity> bookMarkList = companyBookMarkRepository.findByUserAndStatus(user, BookmarkType.BOOKMARK);
+
+        if(bookMarkList.isEmpty()){
+            throw new BookmarkException(NOT_FOUND_BOOKMARK);
+        }
+
+        return bookMarkList.stream()
+                .map(bookMark -> {
+                    Company company = companyRepository.findById(bookMark.getCompany().getCompanyId())
+                            .orElseThrow(()->new RuntimeException("해당 기업을 찾을 수 없습니다."));
+                    return new CompanyBookMarkResponseDto(bookMark);
+                })
                 .collect(Collectors.toList());
     }
-
-    public void removeCompanyBookMark(CompanyBookMarkRequestDto companyBookMarkRequestDto){
-        CompanyBookMarkEntity companyBookMarkEntity = companyBookMarkRepository.findByUserIndexAndCompanyIndex(companyBookMarkRequestDto.getUserIndex(), companyBookMarkRequestDto.getCompanyIndex())
-                .orElseThrow(()->new RuntimeException("해당되는 북마크가 없습니다."));
-        companyBookMarkEntity.setStatus(BookmarkType.BOOKMARK); // 그냥 reposiroty 내에서 삭제하는걸로 변경하쇼
-        companyBookMarkRepository.save(companyBookMarkEntity);
-    }*/
 
     public void addCandidate(UserDetailsImpl userDetails, int cvIndex){
         Company company = companyRepository.findById(userDetails.getId())
