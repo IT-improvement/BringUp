a1bc33ec25d3a4d878de969d1ccd30d39af47fb4
LBJ <pojun406@naver.com>
2024-09-12 17:28:11 +0900

    modify : Bookmark
    북마크쪽 Entity와 DB가 구성이 이상하다는것을 발견하고 DB에 PK로 id를 추가후 JPA에서 사용 가능하게 변경함. 그 후 Entity에는 다른 Entity를 joinColume타입으로 집어넣었고 이를 DTO에서 응용해 사용하는 방식으로 변경

--- src/main/java/com/bringup/common/bookmark/domain/entity/CompanyBookMarkEntity.java
+++ src/main/java/com/bringup/common/bookmark/domain/entity/CompanyBookMarkEntity.java
@@ -1,6 +1,8 @@
 package com.bringup.common.bookmark.domain.entity;
 
 import com.bringup.common.enums.BookmarkType;
+import com.bringup.company.user.entity.Company;
+import com.bringup.member.user.domain.entity.UserEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +16,18 @@ import lombok.Setter;
 public class CompanyBookMarkEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "company_index")
-    private int companyIndex;
+    @Column(name = "id") // 고유 ID 필드 추가
+    private Long id;
 
-    @Column(name = "user_index")
-    private int userIndex;
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "company_index", nullable = false)
+    private Company company;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_index", nullable = false)
+    private UserEntity user;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = "status")
+    @Column(name = "status", nullable = false)
     private BookmarkType status = BookmarkType.BOOKMARK;
 }
