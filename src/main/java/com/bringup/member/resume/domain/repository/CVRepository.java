4599cb3e3fed46bf67303a2ce7ef32a61392a25b
junsung1202 <junsung101010@gmail.com>
2024-09-17 00:52:18 +0900

    ApplyRecruitmentService 수정
    지원했던 공고 리스트 출력 로직 수정

--- src/main/java/com/bringup/member/resume/domain/repository/CVRepository.java
+++ src/main/java/com/bringup/member/resume/domain/repository/CVRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CVRepository extends JpaRepository<CVEntity, Integer> {
@@ -12,4 +13,5 @@ public interface CVRepository extends JpaRepository<CVEntity, Integer> {
 
     List<CVEntity> findAllByUserIndex(int index);
 
+    Optional<CVEntity> findByUserIndex(int userIndex);
 }
