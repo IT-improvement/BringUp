cfdcbbb60449d0bf81a0d6f3d56136d0f1f8eac7
junsung1202 <junsung101010@gmail.com>
2024-09-17 22:07:44 +0900

    ApplyRecruitment 수정
    ApplyRecruitmentController 수정
    리스트 출력 요청 추가
    ApplyRecruitmentService 수정
    지원하기 기능 추가 예정

--- src/main/java/com/bringup/member/applyRecruitment/controller/ApplyRecruitmentController.java
+++ src/main/java/com/bringup/member/applyRecruitment/controller/ApplyRecruitmentController.java
@@ -1,4 +1,24 @@
 package com.bringup.member.applyRecruitment.controller;
 
+import com.bringup.common.security.service.UserDetailsImpl;
+import com.bringup.member.applyRecruitment.domain.service.ApplyRecruitmentService;
+import com.bringup.member.applyRecruitment.dto.response.ApplyRecruitmentResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class ApplyRecruitmentController {
+    private final ApplyRecruitmentService applyRecruitmentService;
+
+    @GetMapping("/mem/applyRecruitment/list")
+    public ResponseEntity<List<ApplyRecruitmentResponseDto>> getApplyRecruitmentList(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        List<ApplyRecruitmentResponseDto> applyRecruitmentList = applyRecruitmentService.getApplyRecruitmentList(userDetails);
+        return ResponseEntity.ok(applyRecruitmentList);
+    }
 }
