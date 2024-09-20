33fad2ef46a5e28c82b5c76479017347298632bc
junsung1202 <junsung101010@gmail.com>
2024-09-20 17:46:13 +0900

    ApplyRecruitment 수정
    ApplyRecruitment Service, Controller 지원하기 기능 추가
    지원 상태(status) 확인하는 ApplyCVType 추가

--- src/main/java/com/bringup/member/applyRecruitment/controller/ApplyRecruitmentController.java
+++ src/main/java/com/bringup/member/applyRecruitment/controller/ApplyRecruitmentController.java
@@ -1,8 +1,10 @@
 package com.bringup.member.applyRecruitment.controller;
 
+import com.bringup.common.response.BfResponse;
 import com.bringup.common.security.service.UserDetailsImpl;
 import com.bringup.member.applyRecruitment.domain.service.ApplyRecruitmentService;
 import com.bringup.member.applyRecruitment.dto.response.ApplyRecruitmentResponseDto;
+import com.sun.net.httpserver.Authenticator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -15,6 +17,12 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ApplyRecruitmentController {
     private final ApplyRecruitmentService applyRecruitmentService;
+
+    public ResponseEntity<BfResponse<?>> addApplyRecruitment(@AuthenticationPrincipal UserDetailsImpl userDetails){
+        applyRecruitmentService.addApplyRecruitment(userDetails);
+        return ResponseEntity.ok(new BfResponse<>("이력서 지원 완료"));
+    }
+
 
     @GetMapping("/mem/applyRecruitment/list")
     public ResponseEntity<List<ApplyRecruitmentResponseDto>> getApplyRecruitmentList(@AuthenticationPrincipal UserDetailsImpl userDetails){
