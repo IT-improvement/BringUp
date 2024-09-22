b7c854e95f66cf42b063dd5440ee5a6ac0f8bd06
LBJ <pojun406@naver.com>
2024-09-22 22:13:21 +0900

    modify : advertisement logic
    광고쪽 로직이 상당히 이상하게 작성되어있었던걸 발견하고 전체적으로 수정하고 또한 controller에서 throw된 값을 프론트로 넘길 수 있도록 수정했다.

--- src/main/java/com/bringup/company/advertisement/dto/request/AdvertisementRequestDto.java
+++ src/main/java/com/bringup/company/advertisement/dto/request/AdvertisementRequestDto.java
@@ -5,9 +5,7 @@ import lombok.Data;
 
 @Data
 public class AdvertisementRequestDto {
-    private Integer advertisementIndex;
     private Integer recruitmentIndex;
-    private String advertisementImage;
     private String type;
     private String displayTime;
 }
