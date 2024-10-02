e6a0f3989d4c351c4338c817872368fdc624cc09
pojun406 <58235899+pojun406@users.noreply.github.com>
2024-10-02 15:51:49 +0900

    add : advertisement
    광고관련 로직을 새로 작성하기 시작했다.

---
+++ src/main/java/com/bringup/company/advertisement/service/BannerAdService.java
@@ -0,0 +1,9 @@
+package com.bringup.company.advertisement.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class BannerAdService {
+}
