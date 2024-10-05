b78e70ebc46e160566c1b7e26ba18911769b65e3
LBJ <pojun406@naver.com>
2024-10-05 20:56:26 +0900

    modify : securityConfig
    재수정

--- src/main/java/com/bringup/common/security/config/SpringSecurityConfig.java
+++ src/main/java/com/bringup/common/security/config/SpringSecurityConfig.java
@@ -170,6 +170,7 @@ public class SpringSecurityConfig {
     private RequestMatcher[] AuthRequestMatchers() {
         List<RequestMatcher> requestMatchers = List.of(
                 antMatcher(POST, "/com/companyName"),               // 기업명 헤더 삽입
+                antMatcher(GET, "/com/companyInfo/post"),           // 회원 정보 조회
                 antMatcher("/com/user/**"),
 
                 antMatcher("/com/recruitment/**"),
