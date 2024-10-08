c3d479cd4e4b4e29bc5afa707421a363e3ae9ee1
Joo-seungjae <jujae04@kyungmin.ac.kr>
2024-10-08 15:52:23 +0900

    Modify PortfolioController: PortfolioResponseDto명 변경

--- src/main/java/com/bringup/member/portfolio/portfolio/controller/PortfolioController.java
+++ src/main/java/com/bringup/member/portfolio/portfolio/controller/PortfolioController.java
@@ -1,11 +1,9 @@
 package com.bringup.member.portfolio.portfolio.controller;
 
-import com.bringup.common.response.ResponseDto;
 import com.bringup.member.portfolio.portfolio.domain.PortfolioService;
 import com.bringup.member.portfolio.portfolio.dto.PortfolioInsertResponseDto;
-import com.bringup.member.portfolio.portfolio.dto.PortfolioReponseDto;
+import com.bringup.member.portfolio.portfolio.dto.PortfolioResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -18,8 +16,8 @@ public class PortfolioController {
     private final PortfolioService portfolioService;
 
     @GetMapping("/list")
-    public ResponseEntity<? super PortfolioReponseDto> getList(@AuthenticationPrincipal String userCode){
-        ResponseEntity<? super PortfolioReponseDto> respone = portfolioService.portfolioList(userCode);
+    public ResponseEntity<? super PortfolioResponseDto> getList(@AuthenticationPrincipal String userCode){
+        ResponseEntity<? super PortfolioResponseDto> respone = portfolioService.portfolioList(userCode);
         return respone;
     }
 
