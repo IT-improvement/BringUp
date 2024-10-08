1728320f0338a939b1a6b4a705c6dae5dbf08ab6
Joo-seungjae <jujae04@kyungmin.ac.kr>
2024-10-08 16:13:05 +0900

    Add ResponseCode,ResponseMessage: EXISTED_EMAIL 변수 추가

--- src/main/java/com/bringup/common/response/ResponseCode.java
+++ src/main/java/com/bringup/common/response/ResponseCode.java
@@ -12,6 +12,7 @@ public interface ResponseCode {
     String NOT_EXISTED_BOARD = "NB";
     String NOT_EXISTED_STUDY = "NS";
     String NOT_EXISTED_REPORT = "NR";
+    String EXISTED_EMAIL="EE";
 
     String SPARE_REPORT_ALREADY_EXIST = "SR";
     String REPORT_ALREADY_FULL = "RF";
