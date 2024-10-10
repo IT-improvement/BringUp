bd59217808cefd28c1a35453147750f2c4425ef4
Joo-seungjae <jujae04@kyungmin.ac.kr>
2024-10-10 13:58:01 +0900

    Modify ChatEntity: regDate만 넣기

--- src/main/java/com/bringup/common/chat/domain/entity/ChatEntity.java
+++ src/main/java/com/bringup/common/chat/domain/entity/ChatEntity.java
@@ -6,17 +6,19 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import java.sql.Timestamp;
+
 @Getter
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 @Table(name = "chat")
-public class ChatEntity extends DateEntity {
+public class ChatEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int chatId;
     private int roomId;
     private int senderUserIndex;
     private String message;
-
+    private Timestamp regDate;
 }
