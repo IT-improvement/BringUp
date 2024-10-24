13551f28a3507234cec43bbc357ef7e58e9313b7
jooseungjae <0719improvement@naver.com>
2024-10-24 11:13:54 +0900

    Create LetterRepository

---
+++ src/main/java/com/bringup/member/portfolio/letter/domain/LetterRepository.java
@@ -0,0 +1,8 @@
+package com.bringup.member.portfolio.letter.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LetterRepository extends JpaRepository<LetterEntity,Integer> {
+}
