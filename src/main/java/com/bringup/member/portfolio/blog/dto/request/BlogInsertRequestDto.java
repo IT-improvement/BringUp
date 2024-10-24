29c73448e35bcf38612f3144ba11414f633f3775
jooseungjae <0719improvement@naver.com>
2024-10-24 10:24:21 +0900

    Create BlogInsertRequestDto

---
+++ src/main/java/com/bringup/member/portfolio/blog/dto/request/BlogInsertRequestDto.java
@@ -0,0 +1,10 @@
+package com.bringup.member.portfolio.blog.dto.request;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BlogInsertRequestDto {
+    private String url;
+}
