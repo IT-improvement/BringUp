66fb2c114fe8f57d115385264eaffa6270380b31
jooseungjae <0719improvement@naver.com>
2024-10-24 14:22:10 +0900

    Add CareerRepository: findByUserIndex method 추가

--- src/main/java/com/bringup/member/portfolio/career/domain/CareerRepository.java
+++ src/main/java/com/bringup/member/portfolio/career/domain/CareerRepository.java
@@ -3,6 +3,9 @@ package com.bringup.member.portfolio.career.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CareerRepository extends JpaRepository<CareerEntity,Integer> {
+    List<CareerEntity> findByUserIndex(int userIndex);
 }
