8309e84c154c890abaab4d89a2f2ee142e57d93c
jooseungjae <0719improvement@naver.com>
2024-10-24 13:52:11 +0900

    Add LetterService, LetterController: insert 로직 추가

--- src/main/java/com/bringup/member/portfolio/letter/domain/LetterService.java
+++ src/main/java/com/bringup/member/portfolio/letter/domain/LetterService.java
@@ -1,7 +1,9 @@
 package com.bringup.member.portfolio.letter.domain;
 
 import com.bringup.common.response.ResponseDto;
+import com.bringup.member.portfolio.letter.dto.request.LetterInsertRequestDto;
 import com.bringup.member.portfolio.letter.dto.response.LetterListResponseDto;
+import com.bringup.member.portfolio.letter.dto.response.LetterResponseDto;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.springframework.http.ResponseEntity;
@@ -25,5 +27,33 @@ public class LetterService {
         }
 
         return LetterListResponseDto.success(letterEntity);
+    }
+
+    public ResponseEntity<? super LetterResponseDto> insertLetter(int userIndex, LetterInsertRequestDto requestDto){
+        LetterEntity letterEntity = null;
+
+        try{
+            letterEntity = letterRepository.findByUserIndex(userIndex);
+        } catch (Exception e) {
+            return ResponseDto.databaseError();
+        }
+
+        if(requestDto.getAnswser1()!=null){
+            letterEntity.setAnswser1(requestDto.getAnswser1());
+        }
+        if(requestDto.getAnswser2()!=null) {
+            letterEntity.setAnswser2(requestDto.getAnswser2());
+        }
+        if(requestDto.getAnswser3()!=null) {
+            letterEntity.setAnswser3(requestDto.getAnswser3());
+        }
+        try{
+            letterRepository.save(letterEntity);
+        }catch (Exception e) {
+            return ResponseDto.databaseError();
+        }
+
+        return LetterResponseDto.success();
+
     }
 }
