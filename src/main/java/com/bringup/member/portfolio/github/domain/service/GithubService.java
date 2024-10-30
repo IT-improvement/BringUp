43fe60dab5e6a1836e280a7b7316f8c318c84f86
jooseungjae <0719improvement@naver.com>
2024-10-30 15:53:17 +0900

    Add GithubController, GithubService: github token insert 및 로그인 한 유저로 github repository 찾기

--- src/main/java/com/bringup/member/portfolio/github/domain/service/GithubService.java
+++ src/main/java/com/bringup/member/portfolio/github/domain/service/GithubService.java
@@ -1,5 +1,9 @@
 package com.bringup.member.portfolio.github.domain.service;
 
+import com.bringup.common.response.ResponseDto;
+import com.bringup.member.portfolio.github.dto.GithubResponseDto;
+import com.bringup.member.user.domain.entity.UserEntity;
+import com.bringup.member.user.domain.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
@@ -9,16 +13,25 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import org.json.JSONObject;
+
+import java.util.Optional;
+
 @Service
 public class GithubService {
 
     private final RestTemplate restTemplate;
+    private final UserRepository userRepository;
 
-    public GithubService(RestTemplate restTemplate) {
+    public GithubService(RestTemplate restTemplate, UserRepository userRepository) {
         this.restTemplate = restTemplate;
+        this.userRepository = userRepository;
     }
 
-    public String getUserData(String githubToken) {
+    public String getUserData(int userCode) {
+
+        Optional<UserEntity> user = userRepository.findByUserIndex(userCode);
+        UserEntity userEntity = user.get();
+        String githubToken = userEntity.getGithubToken();
         String url = "https://api.github.com/user";
 
         HttpHeaders headers = new HttpHeaders();
@@ -30,13 +43,16 @@ public class GithubService {
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         return response.getBody();
     }
-    public String getUserLogin(String githubToken) {
-        String userData = getUserData(githubToken);
+    public String getUserLogin(int userCode) {
+        String userData = getUserData(userCode);
         JSONObject jsonObject = new JSONObject(userData);
         return jsonObject.getString("login");
     }
 
-    public String getOrgRepos(String org,String githubToken) {
+    public String getOrgRepos(String org,int userCode) {
+        Optional<UserEntity> user = userRepository.findByUserIndex(userCode);
+        UserEntity userEntity = user.get();
+        String githubToken = userEntity.getGithubToken();
         String url = "https://api.github.com/users/" + org + "/repos";
 
         HttpHeaders headers = new HttpHeaders();
@@ -48,6 +64,17 @@ public class GithubService {
 
         ResponseEntity<String> response = restTemplate.exchange(url, HttpMethod.GET, entity, String.class);
         return response.getBody();
+    }
+
+    public ResponseEntity<? super GithubResponseDto> insertGithubToken(int userCode,String token){
+        Optional<UserEntity> user = userRepository.findByUserIndex(userCode);
+        if(!user.isPresent()){
+            return ResponseDto.databaseError();
+        }
+        UserEntity userEntity = user.get();
+        userEntity.setGithubToken(token);
+        userRepository.save(userEntity);
+        return GithubResponseDto.success();
     }
 
 }
