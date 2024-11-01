ab37158c28b1a7235362102efa97ac251e65c25b
jooseungjae <0719improvement@naver.com>
2024-11-01 20:48:29 +0900

    Add AwardRepository: existsByOrganizationAndTitleAndAwarDate method 추가

--- src/main/java/com/bringup/member/portfolio/award/domain/AwardRepository.java
+++ src/main/java/com/bringup/member/portfolio/award/domain/AwardRepository.java
@@ -3,6 +3,11 @@ package com.bringup.member.portfolio.award.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface AwardRepository extends JpaRepository<AwardEntity, Integer> {
+
+    boolean existsByOrganizationAndTitleAndAwarDate(String organization, String title, Date awardDate);
+
 }
