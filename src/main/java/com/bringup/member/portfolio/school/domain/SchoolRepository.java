b9409caac5c30426c3bf2e98235d6823464c44ec
jooseungjae <0719improvement@naver.com>
2024-11-01 19:29:31 +0900

    Add SchoolRepository: findByUserIndex method추가

--- src/main/java/com/bringup/member/portfolio/school/domain/SchoolRepository.java
+++ src/main/java/com/bringup/member/portfolio/school/domain/SchoolRepository.java
@@ -3,6 +3,10 @@ package com.bringup.member.portfolio.school.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface SchoolRepository extends JpaRepository<SchoolEntity, Integer> {
+
+    List<SchoolEntity> findByUserIndex(int userIndex);
 }
