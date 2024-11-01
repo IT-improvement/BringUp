6bc349ee1b47004837e3af70221b3f268b14f215
jooseungjae <0719improvement@naver.com>
2024-11-01 20:58:39 +0900

    Add AwardRepository: findByUserIndex method 추가

--- src/main/java/com/bringup/member/portfolio/award/domain/AwardRepository.java
+++ src/main/java/com/bringup/member/portfolio/award/domain/AwardRepository.java
@@ -4,10 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
+import java.util.List;
 
 @Repository
 public interface AwardRepository extends JpaRepository<AwardEntity, Integer> {
 
     boolean existsByOrganizationAndTitleAndAwarDate(String organization, String title, Date awardDate);
-
+    List<AwardEntity> findByUserIndex(int userIndex);
 }
