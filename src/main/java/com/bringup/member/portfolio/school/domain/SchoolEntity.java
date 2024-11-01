77a445c4c5ff572d0773e3c5745c7351047fe314
jooseungjae <0719improvement@naver.com>
2024-11-01 20:12:39 +0900

    Add SchoolEntity: SchoolRequestDto 생성자 추가

--- src/main/java/com/bringup/member/portfolio/school/domain/SchoolEntity.java
+++ src/main/java/com/bringup/member/portfolio/school/domain/SchoolEntity.java
@@ -1,5 +1,6 @@
 package com.bringup.member.portfolio.school.domain;
 
+import com.bringup.member.portfolio.school.dto.SchoolRequestDto;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -32,4 +33,20 @@ public class SchoolEntity {
     private String double_major;
     private BigDecimal grade;
     private BigDecimal maxGrade;
+
+    public SchoolEntity(int userIndex , SchoolRequestDto schoolRequestDto) {
+        this.userIndex = userIndex;
+        this.type = schoolRequestDto.getType();
+        this.schoolName = schoolRequestDto.getSchoolName();
+        this.location = schoolRequestDto.getLocation();
+        this.startDate = schoolRequestDto.getStartDate();
+        this.endDate = schoolRequestDto.getEndDate();
+        this.startStatus = schoolRequestDto.getStartStatus();
+        this.endStatus = schoolRequestDto.getEndStatus();
+        this.department = schoolRequestDto.getDepartment();
+        this.major = schoolRequestDto.getMajor();
+        this.double_major = schoolRequestDto.getDouble_major();
+        this.grade = schoolRequestDto.getGrade();
+        this.maxGrade = schoolRequestDto.getMaxGrade();
+    }
 }
