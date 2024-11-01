a66dfe36e0cfbb02dd224980c2603154d6edc573
jooseungjae <0719improvement@naver.com>
2024-11-01 20:13:07 +0900

    Add SchoolService, SchoolController: school 정보 삽입 or 수정 로직 추가

--- src/main/java/com/bringup/member/portfolio/school/domain/SchoolService.java
+++ src/main/java/com/bringup/member/portfolio/school/domain/SchoolService.java
@@ -2,6 +2,8 @@ package com.bringup.member.portfolio.school.domain;
 
 import com.bringup.common.response.ResponseDto;
 import com.bringup.member.portfolio.school.dto.SchoolListResponseDto;
+import com.bringup.member.portfolio.school.dto.SchoolRequestDto;
+import com.bringup.member.portfolio.school.dto.SchoolResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,10 +17,11 @@ public class SchoolService {
 
     private final SchoolRepository schoolRepository;
 
-    public ResponseEntity<? super SchoolListResponseDto> getShcoolList(int userCode){
+    public ResponseEntity<? super SchoolListResponseDto> getShcoolList(int userCode) {
         List<SchoolEntity> list;
-        try{list = schoolRepository.findByUserIndex(userCode);
-        }catch (Exception e){
+        try {
+            list = schoolRepository.findByUserIndex(userCode);
+        } catch (Exception e) {
             return ResponseDto.databaseError();
         }
 
@@ -26,16 +29,46 @@ public class SchoolService {
         List<SchoolEntity> universityList = new ArrayList<>();
         List<SchoolEntity> graduateList = new ArrayList<>();
 
-        for(SchoolEntity school: list){
+        for (SchoolEntity school : list) {
             String type = school.getType();
-            if(type.equals("고등"))
+            if (type.equals("고등"))
                 highSchool = school;
-            else if(type.equals("석사")||type.equals("박사"))
+            else if (type.equals("석사") || type.equals("박사"))
                 graduateList.add(school);
             else
                 universityList.add(school);
         }
 
-        return SchoolListResponseDto.success(highSchool,universityList,graduateList);
+        return SchoolListResponseDto.success(highSchool, universityList, graduateList);
+    }
+
+    public ResponseEntity<? super SchoolResponseDto> insertOrEditSchool(int userIndex, SchoolRequestDto schoolRequestDto) {
+        String type = schoolRequestDto.getType();
+        boolean check = schoolRepository.existsByUserIndexAndType(userIndex, type);
+        try {
+            if (check) {
+                SchoolEntity school = schoolRepository.findByUserIndexAndType(userIndex, type);
+
+                school.setSchoolName(schoolRequestDto.getSchoolName());
+                school.setDepartment(schoolRequestDto.getDepartment());
+                school.setGrade(schoolRequestDto.getGrade());
+                school.setMaxGrade(schoolRequestDto.getMaxGrade());
+                school.setMajor(schoolRequestDto.getMajor());
+                school.setDouble_major(schoolRequestDto.getDouble_major());
+                school.setLocation(schoolRequestDto.getLocation());
+                school.setStartDate(schoolRequestDto.getStartDate());
+                school.setEndDate(schoolRequestDto.getEndDate());
+                school.setStartStatus(schoolRequestDto.getStartStatus());
+                school.setEndStatus(schoolRequestDto.getEndStatus());
+
+                schoolRepository.save(school);
+            } else {
+                SchoolEntity schoolEntity = new SchoolEntity(userIndex, schoolRequestDto);
+                schoolRepository.save(schoolEntity);
+            }
+        } catch (Exception e) {
+            return ResponseDto.databaseError();
+        }
+        return SchoolResponseDto.success();
     }
 }
