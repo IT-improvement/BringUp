01abfb9f7146714ddf8978d1ecdc565f325d159e
asowjdan <junsung101010@gmail.com>
2024-11-05 15:53:00 +0900

    ApplyRecruitment 수정
    지원했던 공고 리스트에 회사이름, 공고 이름 같이 출력 되게 수정

--- src/main/java/com/bringup/member/applyRecruitment/dto/request/ApplyRecruitmentResponseDto.java
+++ src/main/java/com/bringup/member/applyRecruitment/dto/request/ApplyRecruitmentResponseDto.java
@@ -3,15 +3,13 @@ package com.bringup.member.applyRecruitment.dto.request;
 import com.bringup.common.enums.ApplyCVType;
 import com.bringup.member.applyRecruitment.domain.entity.ApplyRecruitmentEntity;
 import com.bringup.member.applyRecruitment.domain.enums.ApplicationType;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import lombok.experimental.SuperBuilder;
 
 import java.time.LocalDateTime;
 
 @Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
@@ -22,5 +20,6 @@ public class ApplyRecruitmentResponseDto {
     private ApplicationType applicationType;
     private ApplyCVType status;
     private LocalDateTime applyCVDate;
-
+    private String companyName; //회사 이름
+    private String recruitmentTitle; //공고 이름
 }
