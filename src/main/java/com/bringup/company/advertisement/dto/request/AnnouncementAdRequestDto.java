a74bdc838f8b7384e86abb99c0235fea72667cb2
LBJ <pojun406@naver.com>
2024-11-10 16:40:50 +0900

    modify : fixerror
    광고관련 에러 해결중..

--- src/main/java/com/bringup/company/advertisement/dto/request/AnnouncementAdRequestDto.java
+++ src/main/java/com/bringup/company/advertisement/dto/request/AnnouncementAdRequestDto.java
@@ -1,5 +1,6 @@
 package com.bringup.company.advertisement.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
 
 import java.time.LocalDate;
@@ -8,6 +9,8 @@ import java.time.LocalDate;
 public class AnnouncementAdRequestDto {
     private int recruitmentIndex; // 채용공고 인덱스
     private int durationDays; // 노출 일자
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate startDate; // 시작 날짜
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private LocalDate endDate; // 끝 날짜
 }
