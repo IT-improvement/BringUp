f2deeac46f1b07a0a0009d7c2b74123a504deb37
jooseungjae <0719improvement@naver.com>
2024-11-16 16:11:48 +0900

    Create Certificate: 어학에 대한 생성, 리스트, 삭제 로직 추가

---
+++ src/main/java/com/bringup/member/portfolio/certificate/domain/CertificateEntity.java
@@ -0,0 +1,31 @@
+package com.bringup.member.portfolio.certificate.domain;
+
+import com.bringup.member.portfolio.certificate.dto.CertificateRequestDto;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "certificate")
+public class CertificateEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int certificateIndex;
+    private int userIndex;
+    private String title;
+    private String issueStatus;
+    private String issueCenter;
+
+    public CertificateEntity(int userIndex, CertificateRequestDto dto){
+        this.userIndex = userIndex;
+        this.title = dto.getTitle();
+        this.issueStatus = dto.getIssueStatus();
+        this.issueCenter = dto.getIssueCenter();
+    }
+}
