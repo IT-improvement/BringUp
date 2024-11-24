fada3b63c7e255dec085926ea3451a44bcb7908f
LBJ <pojun406@naver.com>
2024-11-24 23:43:40 +0900

    modify : review update, accept
    개인쪽 리뷰 업데이트 해주고 지원자 확인해주는 백단 코드 업데이트

--- src/main/java/com/bringup/company/review/repository/InterviewReviewRepository.java
+++ src/main/java/com/bringup/company/review/repository/InterviewReviewRepository.java
@@ -1,12 +1,21 @@
 package com.bringup.company.review.repository;
 
 import com.bringup.company.review.entity.InterviewReview;
+import com.bringup.company.user.entity.Company;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface InterviewReviewRepository extends JpaRepository<InterviewReview, Integer> {
     List<InterviewReview> findAllByCompanyCompanyId(int companyId);
+
+    @Query("SELECT r FROM InterviewReview r WHERE r.company.companyId = :companyIdx ORDER BY (r.ambience + r.difficulty) / 2.0 DESC")
+    List<InterviewReview> findTopByAverageRating(@Param("companyIdx") int companyIdx, Pageable pageable);
+
 }
