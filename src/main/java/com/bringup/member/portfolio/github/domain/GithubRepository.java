6923c2bda89affbd1fb6399712e07b698df1980a
jooseungjae <0719improvement@naver.com>
2024-12-06 01:41:51 +0900

    Github insert 및 read 추가

--- src/main/java/com/bringup/member/portfolio/github/domain/GithubRepository.java
+++ src/main/java/com/bringup/member/portfolio/github/domain/GithubRepository.java
@@ -3,6 +3,9 @@ package com.bringup.member.portfolio.github.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface GithubRepository extends JpaRepository<GithubEntity, Integer> {
+    List<GithubEntity> findByCvIndex(int cvIndex);
 }
